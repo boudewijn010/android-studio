b6a7c8edbdaf61c4f6dbd73166ad49f7f3ebafd1
boudewijn010 <Boudewijnvandamme@gmail.com>
2025-03-25 10:20:19 +0100

    weer een nieuw project

--- app/src/main/java/com/example/rules/LoginActivity.kt
+++ app/src/main/java/com/example/rules/LoginActivity.kt
@@ -1,10 +1,14 @@
 package com.example.rules
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.util.Log
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +19,18 @@ class LoginActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val link = findViewById<TextView>(R.id.tvLinktoregister). setOnClickListener{
+            startActivity(Intent(this, RegisterActivity::class.java))
+        }
+
+        val naamveld = findViewById<EditText>(R.id.etLoginUsername)
+        val passwordveld = findViewById<EditText>(R.id.etloginPassword)
+
+        if (naamveld.text.isNotEmpty() && passwordveld.text.isNotEmpty()) {
+            log.d("Route", naamveld.text.toString())
+            log.d("Route", passwordveld.text.toString().sha256Hash())
         }
     }
 }
